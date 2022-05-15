7095322a2a08a7f2f6cdf3cd1af71f83c89fae90
kim <82406962+KristinaKim6686@users.noreply.github.com>
2022-05-15 20:49:31 +0300

    hw 5 done

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 
-
 const userSchema = Schema(
   {
     name: {
@@ -21,6 +20,10 @@ const userSchema = Schema(
     token: {
       type: String,
       default: null,
+    },
+    avatarURL: {
+      type: String,
+      required: true,
     },
   },
   { versionKey: false, timestamps: true }
